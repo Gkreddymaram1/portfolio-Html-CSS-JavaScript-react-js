66aa4c608562d3fcbd1ab5e6144c974ab46535dd
gopalmaram <gopalkrishnareddymaram09@gmail.com>
2026-01-28 12:13:11 +0530

    Refactor App.jsx for cleaner routing and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import {Routes,Route } from 'react-router-dom'; 
 import './App.css'
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact'
-import {Routes,Route } from 'react-router-dom'; 
 import Navbar from './Components/Navbar';
 import User from './Components/user';
 import Footer from './Components/Footer'
@@ -17,10 +17,8 @@ export default function App() {
 
 
 <>
-
-
+  
 <Navbar />
-
 
 <HeroSection/>
 <Routes>
@@ -30,21 +28,11 @@ export default function App() {
 <Route path='/Contact' element={ <Contact/>}/>
 <Route path='/User' element={ <User/>}/>
 <Route path='/Skills' element={ <Skills/>}/>
- 
-
- <Route path='/Projects' element={ <Projects/>}/>
-
-
+  <Route path='/Projects' element={ <Projects/>}/>
 <Route path='*' element={<h1>Not Found data</h1>}/>
-
-
    </Routes> 
-
 <Footer/>
 </>
-
-
-
 
   )
 }
